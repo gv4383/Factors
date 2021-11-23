53e0c88b3b4ec634ab56a9961498a662fc4144a0
gv4383 <gregory.vargas18@gmail.com>
2021-11-23 12:11:48 -0600

    Initial Commit

---
+++ Factors/FactorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactorsApp.swift
+//  Factors
+//
+//  Created by Goyo Vargas on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FactorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
