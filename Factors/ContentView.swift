b81215a6d7ab73e41a05e40841a68a9bcf580af4
gv4383 <gregory.vargas18@gmail.com>
2021-11-23 14:15:56 -0600

    Create initial view and selector for number of questions

--- Factors/ContentView.swift
+++ Factors/ContentView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfQuestions = [1, 5, 10, 20]
+    @State private var selectedNumberOfQuestions = 0
+    
+    @State private var multiplicationTable = 2
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header: Text("How many questions do you want to do?")) {
+                    Picker("Number of questions", selection: $selectedNumberOfQuestions) {
+                        ForEach(0..<numberOfQuestions.count) { index in
+                            Text("\(numberOfQuestions[index])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationTitle("Factors")
+        }
     }
+    
+//    func generateQuestions(numberOfQuestions: Int) -> [Int] {
+//        guard numberOfQuestions > 0 else {
+//            return []
+//        }
+//
+//        let questions = Array(repeating: 0, count: numberOfQuestions)
+//
+//        return questions.map { _ in
+//            Int.random(in: 0...12)
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
