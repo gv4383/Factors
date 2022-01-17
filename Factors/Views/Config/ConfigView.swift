3d7f0dac4b55a741513b640f9d66b38563dd0c08
gv4383 <gregory.vargas18@gmail.com>
2022-01-16 23:32:47 -0600

    Update UI for config view

--- Factors/Views/Config/ConfigView.swift
+++ Factors/Views/Config/ConfigView.swift
@@ -13,9 +13,9 @@ struct ConfigView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    Section(header: Text("How many questions do you want to do?")) {
+            Form {
+                Section {
+                    VStack {
                         Picker(
                             "Number of questions",
                             selection: $viewModel.selectedNumberOfQuestions
@@ -24,32 +24,27 @@ struct ConfigView: View {
                                 Text("\($0)")
                             }
                         }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                    
-                    Section(header: Text("What multiplication table do you want to practice?")) {
-                        Stepper(
-                            "\(viewModel.multiplicationTable)",
-                            value: $viewModel.multiplicationTable,
-                            in: 1...12
-                        )
                     }
                 }
                 
-                NavigationLink {
-                    QuestionsView(
-                        selectedNumberOfQuestions: viewModel.selectedNumberOfQuestions,
-                        multiplicationTable: viewModel.multiplicationTable
+                Section {
+                    Stepper(
+                        "Multiplication table: \(viewModel.multiplicationTable)",
+                        value: $viewModel.multiplicationTable,
+                        in: 1...12
                     )
-                } label: {
-                    Text("Start practicing")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(.blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
                 
-                Spacer()
+                Section {
+                    NavigationLink {
+                        QuestionsView(
+                            selectedNumberOfQuestions: viewModel.selectedNumberOfQuestions,
+                            multiplicationTable: viewModel.multiplicationTable
+                        )
+                    } label: {
+                        Text("Start practicing")
+                    }
+                }
             }
             .navigationTitle("Factors")
         }
